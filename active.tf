3a4745580d1e96c5a7536f9de10cafbfac31ae2c
wintermute000 <bender@planetexpress.com.au>
2022-09-23 07:35:31 +0000

    fixed handling of blank license file variables and added flex-vm support

--- active.tf
+++ active.tf
@@ -135,7 +135,7 @@ resource "azurerm_virtual_machine" "activefgtvm" {
     admin_password = var.adminpassword
     custom_data = templatefile("${var.bootstrap-active}", {
       type            = var.license_type
-      license_file    = "${path.module}/licenses/${var.license}"
+      license_file    = var.fgtlicense != "" ? "./licenses/${var.fgtlicense}" : ""
       activename      = var.activename
       port1_ip        = var.activeport1
       port1_mask      = var.activeport1mask
@@ -157,6 +157,7 @@ resource "azurerm_virtual_machine" "activefgtvm" {
       rsg             = azurerm_resource_group.myterraformgroup.name
       clusterip       = azurerm_public_ip.ClusterPublicIP.name
       # routename       = azurerm_route_table.private1_rt.name
+      fgtflextoken   = var.fgtflextoken
     })
   }
 
